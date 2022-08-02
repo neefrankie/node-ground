899073c5ce4387fe038e29c315887dc1bb25bc07
weiguo.ni <neefrankie@gmail.com>
2022-08-02 16:33:52 +0800

    add doc

--- src/app.ts
+++ src/app.ts
@@ -5,17 +5,19 @@ import { resolve } from 'path';
 const app = express();
 const port = 3000;
 
+// Configure nunjucks template engine.
 configure(
   [
-    resolve(__dirname, "../views"), 
+    resolve(__dirname, "../views"), // Tempalate search path
   ],
   {
     noCache: process.env.NODE_ENV === "development",
     watch: process.env.NODE_ENV === "development",
-    express: app,
+    express: app, // Set nunjucks as express template engine.
   }
 );
 
+// Serve static file for bootstrap from node_modules.
 app.use(express.static(resolve(__dirname, '../node_modules/bootstrap/dist/css')));
 
 app.get('/', (req, res) => {
