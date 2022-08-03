13dabd7c8a641e103ad95842684e3a191b464377
weiguo.ni <neefrankie@gmail.com>
2022-08-03 17:31:40 +0800

    use multer to handle file upload

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,18 @@
 import express from 'express';
 import { configure } from 'nunjucks';
 import { resolve } from 'path';
+import multer from 'multer';
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function(req, file, cb) {
+      cb(null, resolve(__dirname, '../build/'))
+    },
+    filename: function(req, file, cb) {
+      cb(null, file.originalname);
+    }
+  }),
+});
 
 const app = express();
 const port = 3000;
@@ -23,6 +35,14 @@ app.use(express.static(resolve(__dirname, '../build')));
 
 app.get('/', (req, res) => {
   res.render('index.html')
+});
+
+app.put('/upload', upload.single('apk'), (req, res) => {
+  console.log(req.file);
+  console.log(req.body);
+  res.json({
+    ok: true,
+  })
 });
 
 app.listen(port, () => {
