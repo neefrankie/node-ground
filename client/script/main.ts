d2edd387828b45c18401477e17e8b57c0d277ae0
weiguo.ni <neefrankie@gmail.com>
2022-08-04 16:15:15 +0800

    add progress bar

--- client/script/main.ts
+++ client/script/main.ts
@@ -1,3 +1,108 @@
+type Resp = {
+  body: ArrayBuffer | Blob | Document | object | string;
+  statusCode: number;
+  statusText: string;
+};
+
+class AJAX {
+
+  private xhr = new XMLHttpRequest();
+  private method: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE' = 'GET';
+  private url: string;
+
+  get(url: string): AJAX {
+    this.url = url;
+    this.method = 'GET';
+    return this;
+  }
+
+  post(url: string): AJAX {
+    this.url = url;
+    this.method = 'POST';
+    return this;
+  }
+
+  put(url: string): AJAX {
+    this.url = url;
+    this.method = 'PUT';
+    return this;
+  }
+
+  patch(url: string): AJAX {
+    this.url = url;
+    this.method = 'PATCH';
+    return this;
+  }
+
+  delete(url: string): AJAX {
+    this.url = url;
+    this.method = 'DELETE';
+    return this;
+  }
+
+  onProgress(listener: (event: ProgressEvent<XMLHttpRequestEventTarget>) => any): AJAX {
+    this.xhr.addEventListener('progress', listener);
+    return this;
+  }
+
+  onComplete(listener: (resp: Resp) => void): AJAX {
+    this.xhr.addEventListener('load', () => {
+      listener({
+        body: this.xhr.response,
+        statusCode: this.xhr.status,
+        statusText: this.xhr.statusText,
+      });
+    });
+
+    return this;
+  }
+
+  onAbort(listener: (event: ProgressEvent<XMLHttpRequestEventTarget>) => any): AJAX {
+    this.xhr.addEventListener('abort', listener);
+    return this;
+  }
+
+  onError(listener: (resp: Resp) => void): AJAX {
+    this.xhr.addEventListener('error', () => {
+      listener({
+        body: this.xhr.response,
+        statusCode: this.xhr.status,
+        statusText: this.xhr.statusText,
+      })
+    });
+
+    return this;
+  }
+
+  abort(): AJAX {
+    this.xhr.abort();
+    return this;
+  }
+
+  upload(body: FormData) {
+    this.xhr.open(this.method, this.url);
+    this.xhr.send(body);
+  }
+}
+
+class Progress {
+  private elem: HTMLElement;
+
+  constructor(selector: string) {
+    this.elem = document.querySelector<HTMLElement>(selector)!;
+  }
+
+  reset() {
+    this.elem.style.width = "0";
+  }
+
+  update(percent: number) {
+    this.elem.style.width = `${percent}%`;
+  }
+}
+
+const progress = new Progress('.progress .progress-bar');
+
 const previewEl = document.querySelector<HTMLElement>('.preview');
 const galleryEl = document.getElementById('gallery');
 
@@ -54,6 +159,9 @@ function handleSubmit() {
   const form = document.getElementById('fileForm');
   const inputEl = document.querySelector<HTMLInputElement>('#formFile');
 
+
+  progress.reset();
+
   form?.addEventListener('submit', (e) => {
     e.preventDefault();
 
@@ -73,19 +181,24 @@ function handleSubmit() {
 
     console.log(formData);
 
-    fetch('/upload', {
-        method: 'PUT',
-        body: formData,
-        // headers: {
-        //   'Content-Type': 'multipart/form-data'
-        // }
+    const ajax = new AJAX();
+
+    ajax.put('/upload')
+      .onProgress((e) => {
+        if (e.lengthComputable) {
+          const percentComplete = e.loaded / e.total * 100;
+          progress.update(percentComplete);
+        }
       })
-      .then(resp => {
-        console.log(resp);
+      .onComplete(resp => {
+        console.log('Upload completed');
+        console.log(resp.body);
+        console.log(resp.statusCode);
       })
-      .catch(err => {
-        console.log(err);
-      });
+      .onError(resp => {
+        console.log(`Error: ${resp.statusCode}, ${resp.statusText}`);
+      })
+      .upload(formData);
   });
 }
 
