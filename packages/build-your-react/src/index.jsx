5ca721d91949bf88bc90ed73d0551b56bd809719
weiguo.ni <neefrankie@gmail.com>
2023-04-18 11:29:30 +0800

    upgrade all dependencies

--- packages/build-your-react/src/index.jsx
+++ packages/build-your-react/src/index.jsx
@@ -1,6 +1,12 @@
 import { createRoot } from 'react-dom/client';
 
-const element = <h1 title="foo">Hello</h1>;
+const element = {
+  type: "h1",
+  props: {
+    title: "foo",
+    children: "Hello"
+  }
+}
 
 const root = createRoot(document.getElementById("root"));
 
