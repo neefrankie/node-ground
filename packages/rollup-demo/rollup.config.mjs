87c504b4805d9dda6fcb0b1a7fb3ae5b906fb050
weiguo.ni <neefrankie@gmail.com>
2023-06-26 14:45:21 +0800

    refactor babel config

--- packages/rollup-demo/rollup.config.mjs
+++ packages/rollup-demo/rollup.config.mjs
@@ -2,6 +2,8 @@ import liverealod from 'rollup-plugin-livereload';
 import serve from 'rollup-plugin-serve';
 import { babel } from '@rollup/plugin-babel';
 
+const production = !process.env.ROLLUP_WATCH;
+
 export default {
   input: 'client/main.js',
   output: {
@@ -10,11 +12,13 @@ export default {
     
   },
   plugins: [
-    babel(),
-    serve({
+    babel({
+      babelHelpers: 'bundled'
+    }),
+    !production && serve({
       contentBase: ['public', 'build']
     }),
-    liverealod({
+    !production && liverealod({
       // By default, it watches the current directory.
       watch:['public', 'build'],
     }),
