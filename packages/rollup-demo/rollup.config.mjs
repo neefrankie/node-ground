611c49f3c344c5bba12e634113bc295c4291f3f0
neefrankie <neefrankie@gmail.com>
2023-09-01 14:46:42 +0800

    removed postcss

--- packages/rollup-demo/rollup.config.mjs
+++ packages/rollup-demo/rollup.config.mjs
@@ -1,7 +1,7 @@
 import liverealod from 'rollup-plugin-livereload';
 import serve from 'rollup-plugin-serve';
 import { babel } from '@rollup/plugin-babel';
-import postcss from 'rollup-plugin-postcss';
+// import postcss from 'rollup-plugin-postcss';
 import cssnano from 'cssnano';
 
 const production = !process.env.ROLLUP_WATCH;
@@ -17,14 +17,14 @@ export default {
     babel({
       babelHelpers: 'bundled'
     }),
-    postcss({
-      plugins: [
-        production && cssnano({
-          preset: 'default'
-        })
-      ],
-      extract: true,
-    }),
+    // postcss({
+    //   plugins: [
+    //     production && cssnano({
+    //       preset: 'default'
+    //     })
+    //   ],
+    //   extract: true,
+    // }),
     !production && serve({
       contentBase: ['public', 'build']
     }),
