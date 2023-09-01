27de145a075ee0eed6d7353dab0796348f171d2b
neefrankie <neefrankie@gmail.com>
2023-09-01 22:37:55 +0800

    try css

---
+++ packages/rollup-demo/client/main.js
@@ -0,0 +1,30 @@
+function animateEvent() {
+  const element = document.getElementById("watchme");
+
+  element.addEventListener("animationstart", listener, false);
+  element.addEventListener("animationend", listener, false);
+  element.addEventListener("animationiteration", listener, false);
+
+  element.className = "slidein"
+}
+
+function listener(event) {
+  const li = document.createElement("li");
+  switch (event.type) {
+    case "animationstart":
+      li.textContent = `Started: elapsed time is ${event.elapsedTime}`;
+      break;
+
+    case "animationend":
+      li.textContent = `Ended: elapsed time is ${event.elaspedTime}`;
+      break;
+
+    case "animationiteration":
+      li.textContent = `New loop started at time ${event.elapsedTime}`;
+      break;
+  }
+
+  document.getElementById("output").appendChild(li);
+}
+
+animateEvent();
