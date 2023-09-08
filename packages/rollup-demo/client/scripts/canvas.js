e7fc5445312d5a32b4b045385fa25ef23e95ddc6
neefrankie <neefrankie@gmail.com>
2023-09-08 08:01:33 +0800

    canvas style

--- packages/rollup-demo/client/scripts/canvas.js
+++ packages/rollup-demo/client/scripts/canvas.js
@@ -245,6 +245,54 @@ function drawPath2D() {
   }
 }
 
+function drawPalette() {
+  const ctx = document.getElementById('palette').getContext('2d');
+
+  for (let i = 0; i < 6; i++) {
+    for (let j = 0; j < 6; j++) {
+      ctx.fillStyle = `rgb(${Math.floor(255 - 42.5 * i)}, ${Math.floor(255 - 42.5 * j)}, 0)`;
+
+      ctx.fillRect(j * 25, i * 25, 25, 25);
+    }
+  }
+}
+
+function drawPaletteCicle() {
+  const ctx = document.getElementById('paletteCircle').getContext('2d');
+
+  for (let i = 0; i < 6; i++) {
+    for (let j = 0; j < 6; j++) {
+      ctx.strokeStyle = `rgb(0, ${Math.floor(255 - 42.5 * i)}, ${Math.floor(255 - 42.5 * j)})`;
+
+      ctx.beginPath();
+      ctx.arc(12.5 + j * 25, 12.5 + i * 25, 10, 0, 2 * Math.PI, true);
+      ctx.stroke();
+    }
+  }
+}
+
+function drawGlobalAlpha() {
+  const ctx = document.getElementById('globalAlpha').getContext('2d');
+
+  ctx.fillStyle = '#FD0';
+  ctx.fillRect(0, 0, 75, 75);
+  ctx.fillStyle = '#6C0';
+  ctx.fillRect(75, 0, 75, 75);
+  ctx.fillStyle = '#09F';
+  ctx.fillRect(0, 75, 75, 75);
+  ctx.fillStyle = '#F30';
+  ctx.fillRect(75, 75, 75, 75);
+  ctx.fillStyle = '#FFF';
+
+  ctx.globalAlpha = 0.2;
+
+  for (let i = 0; i < 7; i++) {
+    ctx.beginPath();
+    ctx.arc(75, 75, 10 + 10 * i, 0, Math.PI * 2, true);
+    ctx.fill();
+  }
+}
+
 window.addEventListener('load', () => {
   draw();
   drawRect();
@@ -256,4 +304,7 @@ window.addEventListener('load', () => {
   drawCubic();
   drawPacMan();
   drawPath2D();
+  drawPalette();
+  drawPaletteCicle();
+  drawGlobalAlpha();
 });
