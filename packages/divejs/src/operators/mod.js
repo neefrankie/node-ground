dad3ebacca8de5392d27c4fe4c36255c9b758dbe
neefrankie <neefrankie@gmail.com>
2023-09-10 21:48:22 +0800

    math.trunc

---
+++ packages/divejs/src/operators/mod.js
@@ -0,0 +1,9 @@
+console.log(`13 % 5 = ${13 % 5}`);
+console.log(`-13 % 5 = ${-13 % 5}`);
+console.log(`4 % 2 = ${4 % 2}`);
+console.log(`-4 % 2 = ${-4 % 2}`);
+
+console.log(`Math.trunc(13.37) = ${Math.trunc(13.37)}`);
+console.log(`Math.trunc(42.84) = ${Math.trunc(42.84)}`);
+console.log(`Math.trunc(0.123) = ${Math.trunc(0.123)}`);
+console.log(`Math.trunc(-0.123) = ${Math.trunc(-0.123)}`);
