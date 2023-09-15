b0401c79f75834b55a654ee1a7f57c0cea557b80
neefrankie <neefrankie@gmail.com>
2023-09-15 11:04:16 +0800

    add cats module

--- packages/nest-demo/src/cats/cats.controller.ts
+++ packages/nest-demo/src/cats/cats.controller.ts
@@ -1,32 +1,65 @@
-import { Controller, Get, Post, Header } from '@nestjs/common';
-
-type ICat = {
-  id: number;
-  name: string;
-  breed: string;
-};
+import {
+  Controller,
+  Get,
+  Post,
+  Header,
+  Param,
+  Body,
+  Res,
+  Put,
+  Delete,
+  HttpStatus,
+} from '@nestjs/common';
+import { Response } from 'express';
+import {
+  Cat,
+  createNewCat,
+  findCat,
+  updateCat,
+} from './interfaces/cat.interface';
+import { CatsService } from './cats.service';
+import { CreateCatDto } from './dto/create-cat.dto';
 
 @Controller('cats')
 export class CatsController {
+  constructor(private catsService: CatsService) {}
+
   @Post()
-  create(): string {
-    return 'This action adds a new cat';
+  async create(@Body() createCatDto: CreateCatDto): Promise<Cat> {
+    const cat = await createNewCat(createCatDto);
+    this.catsService.create(createCatDto);
+    return cat;
   }
 
   @Get()
   @Header('Access-Control-Allow-Origin', '*')
-  findAll(): ICat[] {
-    return [
-      {
-        id: 1,
-        name: 'Hello',
-        breed: 'a',
-      },
-      {
-        id: 2,
-        name: 'World',
-        breed: 'b',
-      },
-    ];
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateCatDto: CreateCatDto) {
+    updateCat(id, updateCatDto);
+    return `This action updates a #${id} cat`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes a #${id} cat`;
+  }
+
+  @Get(':id')
+  findOne(
+    @Param() params: any,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    console.log(params.id);
+    const cat = findCat(params.id);
+
+    if (cat) {
+      response.send(cat);
+    } else {
+      response.status(HttpStatus.NOT_FOUND);
+    }
   }
 }
