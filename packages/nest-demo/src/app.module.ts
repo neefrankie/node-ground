a33cab5a858f976a975e6f1780dcee16ee501ccf
neefrankie <neefrankie@gmail.com>
2023-09-15 14:20:40 +0800

    removed unused files

--- packages/nest-demo/src/app.module.ts
+++ packages/nest-demo/src/app.module.ts
@@ -1,13 +1,11 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
 
 @Module({
   imports: [CatsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
