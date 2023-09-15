d55cd100a30e1107c60daf179f86297395fb12d2
neefrankie <neefrankie@gmail.com>
2023-09-15 18:03:58 +0800

    try graphql

--- packages/nest-demo/src/app.module.ts
+++ packages/nest-demo/src/app.module.ts
@@ -1,9 +1,32 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { CatsModule } from './cats/cats.module';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { RecipesModule } from './recipes/recipes.module';
+import { upperDirectiveTransformer } from './common/directives/upper-case.directive';
+import { DirectiveLocation, GraphQLDirective } from 'graphql';
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    CatsModule,
+    RecipesModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      transformSchema: (schema) => upperDirectiveTransformer(schema, 'upper'),
+      installSubscriptionHandlers: true,
+      buildSchemaOptions: {
+        directives: [
+          new GraphQLDirective({
+            name: 'upper',
+            locations: [DirectiveLocation.FIELD_DEFINITION],
+          }),
+        ],
+      },
+    }),
+  ],
   controllers: [],
   providers: [],
 })
