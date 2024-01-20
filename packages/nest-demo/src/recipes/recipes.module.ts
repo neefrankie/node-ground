2f5bbe2b0405f208144404e756a3b15e744f3e45
neefrankie <neefrankie@gmail.com>
2024-01-20 19:01:40 +0800

    removed nest-demo

--- packages/nest-demo/src/recipes/recipes.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RecipesService } from './recipes.service';
-import { RecipesResolver } from './recipes.resolver';
-import { DateScalar } from 'src/common/scalars/date.scalar';
-
-@Module({
-  providers: [RecipesService, RecipesResolver, DateScalar],
-})
-export class RecipesModule {}
