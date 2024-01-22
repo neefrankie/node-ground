1a493e3d3ce8d51382fa3ea2f6ddc41d99a26cb3
neefrankie <neefrankie@gmail.com>
2024-01-22 18:06:40 +0800

    remove react

--- packages/webapi/src/server.ts
+++ packages/webapi/src/server.ts
@@ -4,7 +4,6 @@ import { resolve } from 'path';
 import ws from 'ws';
 import { homePageCtx } from './server/model/page';
 import apiRouter from './server/api';
-import { renderAppLogin } from './page/ssr';
 
 const app = express();
 const port = 3000;
@@ -31,9 +30,6 @@ app.get('/login', (req, res) => {
 });
 app.get('/upload', (req, res) => {
   res.render('upload.html');
-});
-app.get('/ssr', (req, res) => {
-  renderAppLogin(res);
 });
 app.get('/mathjax', (req, res) => {
   res.render('mathjax.html', {title: 'MathJax Playground'})
