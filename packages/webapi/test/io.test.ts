a6bf69aaf9a0e22bcc3b88c0bfcf9a6c20a49f4f
neefrankie <neefrankie@gmail.com>
2024-01-24 22:50:59 +0800

    test

--- packages/webapi/test/io.test.ts
+++ packages/webapi/test/io.test.ts
@@ -1,11 +1,21 @@
-import { mkdir } from 'fs/promises';
+import { mkdir, readdir } from 'fs/promises';
 import { join } from 'node:path';
 
 describe('make directory', () => {
-  test('make recursive directory',async () => {
+  test('make recursive directory', async () => {
     const projectFolder = join(__dirname, 'test', 'project');
     const createDir = await mkdir(projectFolder, { recursive: true});
 
     console.log(`created ${createDir}`);
+  });
+});
+
+describe('read dir', () => {
+  test('read directory', async () => {
+    const files = await readdir(__dirname);
+
+    for (const file of files) {
+      console.log(file);
+    }
   });
 });
