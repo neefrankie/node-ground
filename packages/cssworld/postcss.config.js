d7c9990c77daa8f184075161949255dddcc18e80
neefrankie <neefrankie@gmail.com>
2024-01-28 14:00:22 +0800

    remove css dir

--- packages/cssworld/postcss.config.js
+++ packages/cssworld/postcss.config.js
@@ -1,10 +1,8 @@
 const postCssPresetEnv = require('postcss-preset-env');
-const tailwindcss = require('tailwindcss');
 
 /** @type {import('postcss-load-config').Config} */
 module.exports = {
   plugins: [
-    tailwindcss(),
     /* other plugins */
 		/* remove autoprefixer if you had it here, it's part of postcss-preset-env */
     postCssPresetEnv()
