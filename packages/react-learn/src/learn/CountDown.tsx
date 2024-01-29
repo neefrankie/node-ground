9fcd5076eb7bb294e7aecd33cb4c649bf1e5206e
neefrankie <neefrankie@gmail.com>
2024-01-29 15:43:17 +0800

    count down

---
+++ packages/react-learn/src/learn/CountDown.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { useEffect, useRef, useState } from 'react';
+
+export function useInterval(callback: Function, delay: number | null) {
+  const savedCallback = useRef<Function>();
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    function tick() {
+      if (!savedCallback.current) {
+        return;
+      }
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = window.setInterval(tick, delay);
+
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
+
+export function CounterButton(
+  props: {
+    from: number;
+    onFinish: () => void;
+  }
+) {
+
+  const [count, setCount] = useState(props.from);
+  const [isRunning, setIsRunnnig] = useState(true);
+
+  useInterval(() => {
+    console.log(`Counting ${count}`);
+    setCount(count - 1);
+    if (count <= 1) {
+      setIsRunnnig(false);
+      props.onFinish();
+    }
+  }, isRunning ? 1000 : null);
+
+  return (
+    <button
+      className='btn btn-outline-secondary'
+      type="button"
+      disabled={true}
+    >
+      {count}s
+    </button>
+  );
+}
