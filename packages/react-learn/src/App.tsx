cf3b628969fafcdc79411744fa7153b35fb3937a
neefrankie <neefrankie@gmail.com>
2024-01-29 15:43:36 +0800

    hook form

--- packages/react-learn/src/App.tsx
+++ packages/react-learn/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { LoginPage } from './page/LoginPage';
 import { LearnPage } from './page/LearnPage';
-import { RHFPage } from './page/RHF';
+import { RHFPage } from './rhf/RHF';
 
 function App() {
   return (
