97c0376a85b944045b19ca210b6666de4d19f324
neefrankie <neefrankie@gmail.com>
2024-01-30 19:15:54 +0800

    submit button

---
+++ packages/react-learn/src/form/SubmitButton.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Spinner } from '../component/Spinner'
+
+function SpinButton(
+  props: {
+    type?: "submit" | "reset" | "button" | undefined;
+    disabled?: boolean;
+    progress?: boolean;
+    text: string;
+  }
+) {
+  return (
+    <button
+      type={props.type}
+      className='btn btn-primary'
+      disabled={props.disabled}
+    >
+      {
+        props.progress ?
+        <Spinner size='sm'/> :
+        props.text
+      }
+    </button>
+  );
+}
+
+export function SubmitButton(
+  props: {
+    isDirty?: boolean;
+    isValid?: boolean;
+    isSubmitting: boolean;
+    text: string;
+  }
+) {
+  
+  return (
+    <div className='d-grid'>
+      <SpinButton
+        type='submit'
+        disabled={!props.isDirty || !props.isValid || props.isSubmitting}
+        progress={props.isSubmitting}
+        text={props.text}
+      />
+    </div>
+  )
+}
