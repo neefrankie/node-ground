54680ebe605f749ad2e97c8f7e64b60730e3e519
neefrankie <neefrankie@gmail.com>
2024-01-30 21:03:43 +0800

    add mobile form

--- packages/react-learn/src/rhf/HFPage.tsx
+++ packages/react-learn/src/rhf/HFPage.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { SimpleForm } from './SimpleForm';
 import { IDForm } from './IDForm';
+import { MobileForm } from './MobileForm';
 
 export function RHFPage() {
   return (
     <>
-      <h2>Plain Hook Form</h2>
+      <h2>Plain Form</h2>
       <SimpleForm />
+      <h2 className='mt-5'>Mobile Login</h2>
+      <MobileForm />
       <h2 className='mt-5'>ID Card</h2>
       <IDForm />
     </>
