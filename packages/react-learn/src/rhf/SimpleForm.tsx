c09cbc109c35a7f7c1b356bc3e9399c0625218e2
neefrankie <neefrankie@gmail.com>
2024-01-30 19:16:36 +0800

    refactor

--- packages/react-learn/src/rhf/SimpleForm.tsx
+++ packages/react-learn/src/rhf/SimpleForm.tsx
@@ -3,6 +3,7 @@ import { useForm, SubmitHandler } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { sleep } from '../util/sleep';
 import { z } from 'zod';
+import { SubmitButton } from '../form/SubmitButton';
 
 const loginSchema = z.object({
   email: z.string().email().trim(),
@@ -16,12 +17,12 @@ type ILoginValues = {
 };
 
 export function SimpleForm() {
-
+  // formState.isDirty only works if you set initial value here.
   const {
     register,
     handleSubmit,
     watch,
-    formState: { errors, isSubmitting, isDirty, isValid, touchedFields }
+    formState: { errors, isSubmitting, isDirty, isValid, dirtyFields }
   } = useForm<ILoginValues>({
     mode: 'onChange',
     defaultValues: {
@@ -37,14 +38,9 @@ export function SimpleForm() {
   };
 
   console.log(watch('email'));
-  console.log(touchedFields);
-  console.log(errors);
 
-  const emailValid = !(touchedFields.email && errors.email);
-  const pwValid = !(touchedFields.password && errors.password);
-
-  console.log('email valid: ', emailValid);
-  console.log('pwValid: ', pwValid);
+  const emailErr = dirtyFields.email ? errors.email?.message : undefined;
+  const pwErr = dirtyFields.password ? errors.password?.message : undefined;
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -52,38 +48,34 @@ export function SimpleForm() {
       <div className='mb-3'>
         <label htmlFor="">Email</label>
         <input 
-          className={`form-control${emailValid ? '' : ' is-invalid'}`}
+          className={`form-control${emailErr ? ' is-invalid' : ''}`}
           {...register('email')} 
         />
         {
-          errors.email &&
-          <div className="invalid-feedback">{errors.email.message}</div>
+          emailErr &&
+          <div className="invalid-feedback">{emailErr}</div>
         }
       </div>
       
       <div className='mb-3'>
         <label htmlFor="">Password</label>
         <input
-          className={`form-control${pwValid ? '' : ' is-invalid'}`}
+          className={`form-control${pwErr ? ' is-invalid' : ''}`}
           {...register('password')}
         />
         {
-          errors.password &&
-          <div className="invalid-feedback">{errors.password.message}</div>
+          pwErr &&
+          <div className="invalid-feedback">{pwErr}</div>
         }
       </div>
 
-      <button 
-        type='submit'
-        className='btn btn-primary'
-        disabled={!isDirty || !isValid || isSubmitting}
-      >
-        {
-          isSubmitting ?
-          'Submitting...' :
-          'Submit'
-        }
-      </button>
+      <SubmitButton 
+        isDirty={isDirty}
+        isValid={isValid}
+        isSubmitting={isSubmitting}
+        text='Login'
+      />
+
     </form>
   )
 }
