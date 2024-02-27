47149d0a5e28972764e8e0dda8024f30cd255499
neefrankie <neefrankie@gmail.com>
2024-02-27 09:42:22 +0800

    refactor

--- packages/webapi/webpack.config.js
+++ packages/webapi/webpack.config.js
@@ -7,6 +7,8 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
   mode: prod ? 'production' : 'development',
   entry: {
+    auth: './src/client/auth.ts',
+    upload: './src/client/upload.ts',
     canvas: './src/client/canvas.ts',
   },
   devtool: prod ? 'source-map' : 'inline-source-map',
